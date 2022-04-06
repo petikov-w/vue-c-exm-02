9877c65f5594f2b9e053c6548c8739f0bc9fa43c
Petikov Vladimir <developer-krox@yandex.ru>
2022-04-06 18:31:06 +0300

    commit-060422-1835 Работа над блоком отзывов

--- src/components/TestimonialsList.vue
+++ src/components/TestimonialsList.vue
@@ -1,12 +1,49 @@
 <template>
 <div class="testimonials-section">
-
+  <TestimonialsListItem class="west" v-for="testimonial in testimonials" :key="testimonial.id" :tm_data="testimonial">
+  </TestimonialsListItem>
 </div>
 </template>
 
 <script>
+import TestimonialsListItem from '../components/TestimonialsListItem';
 export default {
-  name: "TestimonialsList"
+  name: "TestimonialsList",
+  components: {
+    TestimonialsListItem
+  },
+  data: function () {
+    return {
+      testimonials: [
+        {
+          id: 1,
+          img_src: require('../assets/images/otz2.jpg'),
+          img_alt: "otziv",
+          otziv: "Покупала у ребят коврики для Хундай солярис в подарок молодому человеку. Все очень понравилось. " +
+                 "Буду советовать только вас.",
+          author: "Вера К."
+        },
+        // {
+        //   id: 2,
+        //   img_src: require('../assets/images/otlichie.jpg'),
+        //   img_alt: "otlichie",
+        //   title: "отличия",
+        //   description: "Резиновые коврики загибаются и лезут на педали. EVA коврики такого не допустят! " +
+        //       "Специальные липучки держат его в одном положении. При желании, для водителя устанавливается подпятник " +
+        //       "из рельефного алюминия."
+        // },
+        // {
+        //   id: 3,
+        //   img_src: require('../assets/images/rascvetka.jpg'),
+        //   img_alt: "rascvetka",
+        //   title: "расцветка",
+        //   description: "Автомобильные коврики EVA можно заказать и в индивидуальных размерах и в разных цветах, " +
+        //       "которые комбинируется на выбор покупателя с такими же цветами окантовочной полипропиленовой " +
+        //       "ременной ленты."
+        // }
+      ]
+    }
+  }
 }
 </script>
 
@@ -14,5 +51,8 @@ export default {
     .testimonials-section {
         height: 360px;
         background: linear-gradient(94.31deg, rgba(212, 20, 90, 0.25) 0.96%, rgba(46, 49, 146, 0.25) 104.83%);
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 </style>
---
+++ src/components/TestimonialsListItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="testimonial">
+    <img :src=" tm_data.img_src" :alt=" tm_data.img_alt">
+    <div class="otziv">{{ tm_data.otziv }}</div>
+    <div class="author">{{  tm_data.author }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestimonialsListItem",
+  props: {
+    tm_data: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.testimonial {
+  position: relative;
+  background: #FFFFFF;
+  border: 1px solid #f5f5f5;
+  width: 536px;
+  max-height: 210px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  //overflow: hidden;
+  border-radius: 9px;
+  box-shadow: 0px 1px 20px 6px rgba(236, 198, 217, 0.5);
+  //margin-bottom: 30px; // Потом убрать!
+  padding: 0 46px;
+  padding-top: 80px;
+  img {
+    @include wh_size(90px, 90px, "circle");
+    position: absolute;
+    top: -50px;
+  }
+  .otziv {
+    @include font(18px, 300, 22px);
+    margin: 20px 0 15px 0;
+    text-align: center;
+  }
+  .author {
+    @include font(13px, 300, 16px, #767676);
+    padding: 0 40px;
+    max-width: 290px;
+    text-align: center;
+  }
+}
+</style>
